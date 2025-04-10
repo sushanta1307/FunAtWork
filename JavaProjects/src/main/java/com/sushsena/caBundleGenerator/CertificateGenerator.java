941bcdc280cf8ec992f9405f2cccf723f1734ca6
Sushanta Senapati <sushanta.senapati@oracle.com>
2025-04-10 21:09:18 +0530

    Restructured the java project

---
+++ JavaProjects/src/main/java/com/sushsena/caBundleGenerator/CertificateGenerator.java
@@ -0,0 +1,83 @@
+package com.sushsena.caBundleGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.FileOutputStream;
+import java.math.BigInteger;
+import java.security.*;
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+import java.util.Date;
+import javax.security.auth.x500.X500Principal;
+
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateHolder;
+import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.cert.CertIOException;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+@SpringBootApplication
+public class CertificateGenerator {
+	public static void generateKeyPairAndCABundle(String keyStoreFilePath, String keyStorePassword) throws Exception {
+		// Bouncy Castle as the security provider
+		Security.addProvider(new BouncyCastleProvider());
+	
+		// Generate the key pair
+		KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+		keyPairGenerator.initialize(2048, new SecureRandom());
+		KeyPair keyPair = keyPairGenerator.generateKeyPair();
+	
+		// Define the certificate validity
+		long notBefore = System.currentTimeMillis();
+		long notAfter = notBefore + 30*24*60*60*1000L; // One month validity
+	
+		X500Principal subject= new X500Principal("CN=Sushanta, ST=Odisha, C=India");
+		X500Principal signedByPrincipal = subject;
+		BigInteger certSerialNumber = BigInteger.valueOf(System.currentTimeMillis());
+	
+		// Create the certificate Builder
+		ContentSigner contentSigner = new JcaContentSignerBuilder("SHA256WithRSAEncryption").build(keyPair.getPrivate());
+		X509v3CertificateBuilder certBuilder = new JcaX509v3CertificateBuilder(
+			signedByPrincipal, certSerialNumber, new Date(notBefore), new Date(notAfter), subject, keyPair.getPublic());
+		X509CertificateHolder certHolder = certBuilder.build(contentSigner);
+		JcaX509CertificateConverter converter = new JcaX509CertificateConverter().setProvider("BC");
+		X509Certificate certificate = converter.getCertificate(certHolder);
+	
+		// Save the CA certificate and private key in a KeyStore
+		KeyStore keyStore = KeyStore.getInstance("JKS");
+		keyStore.load(null, null); // Initialize an empty keystore
+	
+		// Set up the KeyStore entry for the CA certificate
+		KeyStore.PrivateKeyEntry entry = new KeyStore.PrivateKeyEntry(keyPair.getPrivate(), new Certificate[] {certificate});
+		KeyStore.PasswordProtection password = new KeyStore.PasswordProtection(keyStorePassword.toCharArray());
+		keyStore.setEntry("myCA", entry, password);
+	
+		FileOutputStream fileName = new FileOutputStream(keyStoreFilePath);
+		keyStore.store(fileName, keyStorePassword.toCharArray());
+	
+		System.out.println("CA Bundle genrated and saved to " + keyStoreFilePath);
+	
+	  }
+	
+	public static void main(String[] args) {
+		// ConfigurableApplicationContext context = SpringApplication.run(CertificateGenerator.class, args);
+		
+		// CertificateGenerator bean = context.getBean(CertificateGenerator.class);
+		
+		// bean.run();
+		try { 
+			CertificateGenerator.generateKeyPairAndCABundle("ca-keystore.jks", "changeit");
+		} catch (Exception e) {
+		    e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ JavaProjects/src/test/java/com/sushsena/caBundleGenerator/CertificateGeneratorTest.java
@@ -0,0 +1,13 @@
+package com.sushsena.caBundleGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificateGeneratorTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
